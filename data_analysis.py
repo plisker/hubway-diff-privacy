80b4a26181230fce59274d14c365b9305c026e64
Paul Lisker <plisker@gmail.com>
2016-07-21 16:18:40 -0400

    more minor changes

--- data_analysis.py
+++ data_analysis.py
@@ -11,17 +11,3 @@ import csv
 from datetime import datetime, timedelta
 import copy
 
-class DataProcessing(object):
-
-	def __init__(self, raw=True):
-		super(DataProcessing, self).__init__()
-		self.raw = raw
-
-	def raw_analysis(self):
-		pass
-
-	def synthetic_analysis(self):
-		pass
-
-	def analysis(self):
-		pass
