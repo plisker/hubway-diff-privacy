97bc412c40b4a56c5777893825fce0e87f2aac0c
Paul Lisker <plisker@gmail.com>
2016-07-22 11:38:57 -0400

    More methods, further development of class

--- data_analysis.py
+++ data_analysis.py
@@ -15,14 +15,13 @@ import copy
 
 class GraphTrips(object):
 	"""docstring for GraphTrips"""
-	def __init__(self, raw=True, filename="", file="", synthetic_file=0.0):
+	def __init__(self, raw=True, filename="raw", file="Data/hubway-syn-data/hubway-error-free.data", synthetic_file="0.0"):
 		super(GraphTrips, self).__init__()
 		self.STATIONS = 145+1 # So as to 1-index
 		self.HOURS = 24 # So as to have first number be the row index
 		self.SET_OF_STATIONS = set([3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145])
 		self.raw = raw
 		self.number_of_trips = 0
-
 		self.filename = filename
 		self.file = file
 		self.synthetic_file = synthetic_file
@@ -43,14 +42,26 @@ class GraphTrips(object):
 					matrix[i][j] = (matrix[i][j])/float(denominator)
 
 	def analysis(self):
-		self.startProgress("Processing data")
-
 		self.in_matrix = self.initialize_matrix()
 		self.out_matrix = self.initialize_matrix()
 		self.in_stations_represented = []
 		self.out_stations_represented = []
 		self.in_dates = []
 		self.out_dates = []
+
+
+		if self.synthetic_file not in ["0.0", "0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1.0"]:
+			self.user_input()
+		else:
+			if self.synthetic_file in ["0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1.0"]:
+				self.file = "Data/hubway-syn-data/"+self.synthetic_file+"-1-hubway-synthetic-our.data"
+				self.filename = self.synthetic_file
+				self.raw = False
+
+		if self.raw:
+			self.startProgress("Processing raw data")
+		else:
+			self.startProgress("Processing "+self.synthetic_file+" synthetic data")
 
 		row_counter = 0
 		with open(self.file, 'rb') as csvfile:
@@ -147,11 +158,26 @@ class GraphTrips(object):
 				self.filename = self.synthetic_file
 				break
 
-	def station_data(self, station_id, bikes_in, bikes_out):
-		station_out = bikes_out[station_id]
+	def user_plot(self):
+		self.continue_running = True
+		while self.continue_running:
+			try:
+				station = input("What station do you want to plot? ")
+				self.plot_station(station, self.in_matrix, self.out_matrix)
+			except:
+				print("That isn't a valid number. Please try again!")
+				continue
+			test = self.plot_again()
+			if test is 0:
+				continue
+			else:
+				break
+
+	def station_data(self, station_id):
+		station_out = self.out_matrix[station_id]
 		del station_out[-1]
 
-		station_in = bikes_in[station_id]
+		station_in = self.in_matrix[station_id]
 		del station_in[-1]
 
 		a = copy.deepcopy(station_in)
@@ -163,17 +189,17 @@ class GraphTrips(object):
 		station_in.append(station_id)
 
 		return x
-
-	def plot_station(self, station_id, bikes_in, bikes_out):
+		
+	def plot_station(self, station_id):
 		hours_axis = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
 
-		station_out = bikes_out[station_id]
+		station_out = self.out_matrix[station_id]
 		del station_out[-1]
 		print("Bikes out: ", station_out)
 		plt.plot(hours_axis, station_out)
 		station_out.append(station_id)
 
-		station_in = bikes_in[station_id]
+		station_in = self.in_matrix[station_id]
 		del station_in[-1]
 		print("Bikes in: ", station_in)
 		plt.plot(hours_axis, station_in)
@@ -198,6 +224,22 @@ class GraphTrips(object):
 		else:
 			print("Sorry, didn't catch that. Let's try again.")
 			plot_again()
+
+	def statistics(self):
+		print("")
+		print("Let's start with some statistics:")
+		if self.raw:
+			print("Data analyzed: raw data")
+		else:
+			print("Data analyzed: "+self.synthetic_file+" synthetic data")
+		print("The number of trips is "+str(self.number_of_trips))
+		print("The number of days for which there are outbound trips are "+str(len(self.out_date_set)))
+		print("The number of days for which there are inbound trips are "+str(len(self.in_date_set)))
+		print("Outgoing stations not represented in the data are: ")
+		print(list(self.out_sets_missing))
+		print("Incoming stations not represented in the data are: ")
+		print(list(self.in_sets_missing))
+		print("")
 
 	# Progress bar with credit to http://stackoverflow.com/a/6169274 #
 	def startProgress(self, title):
--- in_out_graphs.py
+++ in_out_graphs.py
@@ -14,33 +14,22 @@ import sys
 import copy
 from data_analysis import GraphTrips
 
-def printStatistics(data):
-	print("")
-	print("Let's start with some statistics:")
-	print("The number of trips is "+str(data.number_of_trips))
-	print("The number of days for which there are outbound trips are "+str(len(data.out_date_set)))
-	print("The number of days for which there are inbound trips are "+str(len(data.in_date_set)))
-	print("Outgoing stations not represented in the data are: ")
-	print(list(data.out_sets_missing))
-	print("Incoming stations not represented in the data are: ")
-	print(list(data.in_sets_missing))
-	print("")
-
 def main():
+	rawData = GraphTrips()
+	syn1 = GraphTrips(synthetic_file="1.0")
 
-	analyzeData = GraphTrips()
-
-	analyzeData.user_input()
-	analyzeData.analysis()
+	rawData.analysis()
+	syn1.analysis()
 	
-	# printStatistics(analyzeData)
+	rawData.statistics()
+	syn1.statistics()
 
 	in_data = [["",0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]]
 	out_data = [["",0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]]
 
-	for station in analyzeData.SET_OF_STATIONS:
+	for station in rawData.SET_OF_STATIONS:
 		try:
-			arrive, leave = analyzeData.station_data(station, analyzeData.in_matrix, analyzeData.out_matrix)
+			arrive, leave = rawData.station_data(station, rawData.in_matrix, rawData.out_matrix)
 		except:
 			print(station)
 			continue
@@ -52,12 +41,12 @@ def main():
 		out_data.append(leave)
 
 	# Start writing the CSV without outliers
-	with open('Data/'+analyzeData.filename+'/'+analyzeData.filename+'-bikes_in.csv', 'w') as cleaned_file:
+	with open('Data/'+rawData.filename+'/'+rawData.filename+'-bikes_in.csv', 'w') as cleaned_file:
 	    a = csv.writer(cleaned_file, delimiter=',')
 	    data = in_data
 	    a.writerows(data)
 
-	with open('Data/'+analyzeData.filename+'/'+analyzeData.filename+'-bikes_out.csv', 'w') as cleaned_file:
+	with open('Data/'+rawData.filename+'/'+rawData.filename+'-bikes_out.csv', 'w') as cleaned_file:
 	    a = csv.writer(cleaned_file, delimiter=',')
 	    data = out_data
 	    a.writerows(data)
@@ -66,11 +55,11 @@ def main():
 	while continue_running:
 		try:
 			station = input("What station do you want to plot? ")
-			analyzeData.plot_station(station, analyzeData.in_matrix, analyzeData.out_matrix)
+			rawData.plot_station(station, rawData.in_matrix, rawData.out_matrix)
 		except:
 			print("That isn't a valid number. Please try again!")
 			continue
-		test = analyzeData.plot_again()
+		test = rawData.plot_again()
 		if test is 0:
 			continue
 		else:
